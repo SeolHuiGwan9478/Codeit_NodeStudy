beafa6026e02ecfc4d9fc0fdf53f00f4d5315df0
SeolHuiGwan9478 <shg5977@naver.com>
2023-04-02 01:25:48 +0900

    [Study] : Routing by using http module

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const EventEmitter = require('events');
-const myEmitter = new EventEmitter();
+const http = require('http');
+users = ['Tom', 'Andy', 'Jessica', 'Paul'];
 
-const obj = {type: 'text', data: 'Hello Seolhuigwan', date: '2023-04-01'};
-myEmitter.on('test', (info) => {
-    console.log(info);
+let server = http.createServer(function(request, response){
+    if(request.url === '/'){
+        response.end('<h1>Welcome</h1>');
+    }
+    else if(request.url === '/users'){
+        response.end('<h1>' + users + '</h1>');
+    }
+    else if(request.url.split('/')[1] === 'users'){
+        let userIdx = request.url.split('/')[2];
+        let userName = users[userIdx-1];
+
+        response.end('<h1>' + userName + '</h1>');
+    }
+    else{
+        response.end('<h1>Page Not Available</h1>');
+    }
 });
 
-myEmitter.emit('test', obj);
+server.listen(3000);
